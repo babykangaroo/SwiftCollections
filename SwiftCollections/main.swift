0437ed29d3515c32ab791cb2e460d495d2ada792
babykangaroo <joeygreen@joeys-mbp>
2015-12-29 11:06:33 -0600

    Initial Commit

---
+++ SwiftCollections/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftCollections
+//
+//  Created by Joey Green on 12/29/15.
+//  Copyright © 2015 Joey Green. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
